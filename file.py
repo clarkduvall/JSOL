219448ae2113b01cacf4feef3c678ce02751fa70
Clark DuVall <clark.duvall@gmail.com>
2012-07-12 22:57:58 -0700

    file

--- file.py
+++ file.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# File module.
+# Easily create a module by defining functions and an OPS dict.
+
 import os
 
 def _Open(args):
