bcd6b377616ebc0fdcf115b530f24f02c3806488
Clark DuVall <clark.duvall@gmail.com>
2012-07-12 22:09:49 -0700

    license

--- passer.py
+++ passer.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python
+
+# Copyright 2012 Clark DuVall
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # This program illustrates how a JSOL program can be sent between applications.
 # A fib function is created in the first Eval, and then sent to the second Eval
 # to be run.
 
 import jsol
 import json
-import file
-
-print file.OPS
 
 with open('examples/part1.jsol') as f:
    fib = jsol.Eval(json.load(f)).json()
